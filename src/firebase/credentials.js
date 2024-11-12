5be7399345bac2bdf22d3438d115c176b7366db7
juanmayorquin <juanjmayor@outlook.es>
2024-11-11 22:34:50 -0500

    Primer commit

---
+++ src/firebase/credentials.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCewIXoxyuXAu9FrvAVNPzS9_dV5BF6esI",
+  authDomain: "vroom-co.firebaseapp.com",
+  projectId: "vroom-co",
+  storageBucket: "vroom-co.firebasestorage.app",
+  messagingSenderId: "658293080543",
+  appId: "1:658293080543:web:1bd95c5b1594f47a87c46a"
+};
+
+// Initialize Firebase
+const appFirebase = initializeApp(firebaseConfig);
+
+export default appFirebase;
