d51d582d644d44b15ee40932a69e5715d6955c68
jGarzon13 <juliangarzontoro@gmail.com>
2024-11-18 21:15:56 -0500

    Correcciones de Estilos en LoginForm y Register
    Correcciones pequeñas en colores

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -13,9 +13,9 @@ function LoginForm() {
     e.preventDefault();
     try {
       await signInWithEmailAndPassword(auth, email, password);
-      console.log("User logged in Successfully");
+      console.log("Usuario ingresado exitosamente");
       window.location.href = "/";
-      toast.success("User logged in Successfully", {
+      toast.success("Usuario ingresado exitosamente", {
         position: "top-center",
       });
     } catch (error) {
@@ -30,17 +30,17 @@ function LoginForm() {
     <div className="flex items-center justify-center min-h-screen bg-gray-50">
       <div className="w-full max-w-md bg-white rounded-lg shadow-lg p-6">
         <h3 className="text-center text-2xl font-semibold text-gray-700 mb-6">
-          Login
+          Iniciar sesión
         </h3>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label className="block text-gray-600 text-sm font-medium mb-1">
-              Email address
+              Dirección de correo electrónico
             </label>
             <input
               type="email"
               className="w-full px-4 py-2 border rounded-md text-gray-700 focus:outline-none focus:ring focus:ring-blue-300"
-              placeholder="Enter email"
+              placeholder="Ingrese el correo electrónico"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
@@ -48,12 +48,12 @@ function LoginForm() {
 
           <div className="mb-4">
             <label className="block text-gray-600 text-sm font-medium mb-1">
-              Password
+              Contraseña
             </label>
             <input
               type="password"
               className="w-full px-4 py-2 border rounded-md text-gray-700 focus:outline-none focus:ring focus:ring-blue-300"
-              placeholder="Enter password"
+              placeholder="Ingrese la contraseña"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
@@ -62,17 +62,20 @@ function LoginForm() {
           <div className="mb-4">
             <button
               type="submit"
-              className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition"
+              className="w-full bg-[#e02b0f] hover:bg-[#f20b0b] transition-colors text-white py-2 rounded-md"
             >
-              Submit
+              Enviar
             </button>
           </div>
         </form>
 
         <p className="text-sm text-gray-600 text-center">
-          New user?{" "}
-          <Link to={"/register"} className="text-blue-500 hover:underline">
-            Register Here
+          ¿Nuevo usuario?{" "}
+          <Link
+            to={"/register"}
+            className="text-[#e02b0f] hover:text-[#f20b0b] transition-colors"
+          >
+            Regístrate aquí
           </Link>
         </p>
 
--- src/components/register.jsx
+++ src/components/register.jsx
@@ -3,14 +3,14 @@ import React, { useState } from "react";
 import { auth, db } from "../firebase/credentials";
 import { setDoc, doc } from "firebase/firestore";
 import { toast } from "react-toastify";
-import { useNavigate } from "react-router-dom";  // Importamos useNavigate
+import { useNavigate } from "react-router-dom"; // Importamos useNavigate
 
 function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [fname, setFname] = useState("");
   const [lname, setLname] = useState("");
-  const navigate = useNavigate();  // Usamos useNavigate
+  const navigate = useNavigate(); // Usamos useNavigate
 
   const handleRegister = async (e) => {
     e.preventDefault();
@@ -26,13 +26,13 @@ function Register() {
           photo: "",
         });
       }
-      console.log("User Registered Successfully!!");
-      toast.success("User Registered Successfully!!", {
+      console.log("¡Usuario registrado exitosamente!");
+      toast.success("¡Usuario registrado exitosamente!", {
         position: "top-center",
       });
 
       // Redirigir a la pantalla principal después de un registro exitoso
-      navigate("/");  // Aquí redirigimos a la ruta principal "/"
+      navigate("/"); // Aquí redirigimos a la ruta principal "/"
     } catch (error) {
       console.log(error.message);
       toast.error(error.message, {
@@ -45,17 +45,17 @@ function Register() {
     <div className="flex items-center justify-center min-h-screen bg-gray-50">
       <div className="w-full max-w-md bg-white rounded-lg shadow-lg p-6">
         <h3 className="text-center text-2xl font-semibold text-gray-700 mb-6">
-          Sign Up
+          Registro
         </h3>
         <form onSubmit={handleRegister}>
           <div className="mb-4">
             <label className="block text-gray-600 text-sm font-medium mb-1">
-              First name
+              Nombre
             </label>
             <input
               type="text"
               className="w-full px-4 py-2 border rounded-md text-gray-700 focus:outline-none focus:ring focus:ring-blue-300"
-              placeholder="First name"
+              placeholder="Nombre"
               onChange={(e) => setFname(e.target.value)}
               required
             />
@@ -63,24 +63,24 @@ function Register() {
 
           <div className="mb-4">
             <label className="block text-gray-600 text-sm font-medium mb-1">
-              Last name
+              Apellido
             </label>
             <input
               type="text"
               className="w-full px-4 py-2 border rounded-md text-gray-700 focus:outline-none focus:ring focus:ring-blue-300"
-              placeholder="Last name"
+              placeholder="Apellido"
               onChange={(e) => setLname(e.target.value)}
             />
           </div>
 
           <div className="mb-4">
             <label className="block text-gray-600 text-sm font-medium mb-1">
-              Email address
+              Dirección de correo electrónico
             </label>
             <input
               type="email"
               className="w-full px-4 py-2 border rounded-md text-gray-700 focus:outline-none focus:ring focus:ring-blue-300"
-              placeholder="Enter email"
+              placeholder="Ingresa tu correo"
               onChange={(e) => setEmail(e.target.value)}
               required
             />
@@ -88,12 +88,12 @@ function Register() {
 
           <div className="mb-4">
             <label className="block text-gray-600 text-sm font-medium mb-1">
-              Password
+              Contraseña
             </label>
             <input
               type="password"
               className="w-full px-4 py-2 border rounded-md text-gray-700 focus:outline-none focus:ring focus:ring-blue-300"
-              placeholder="Enter password"
+              placeholder="Ingresa tu contraseña"
               onChange={(e) => setPassword(e.target.value)}
               required
             />
@@ -102,16 +102,19 @@ function Register() {
           <div className="mb-4">
             <button
               type="submit"
-              className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition"
+              className="w-full bg-[#e02b0f] text-white py-2 rounded-md hover:bg-[#f20b0b] transition"
             >
-              Sign Up
+              Registrarse
             </button>
           </div>
         </form>
         <p className="text-sm text-gray-600 text-center">
-          Already registered?{" "}
-          <a href="/login" className="text-blue-500 hover:underline">
-            Login
+          ¿Ya tienes cuenta?{" "}
+          <a
+            href="/login"
+            className="text-[#e02b0f] hover:text-[#f20b0b] transition-colors"
+          >
+            Iniciar sesión
           </a>
         </p>
       </div>
